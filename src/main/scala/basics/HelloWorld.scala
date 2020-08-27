e3a6d04dfeea2aaad0b46c7af4ad7c4a70a0cd4c
Mark Lewis <mlewis@trinity.edu>
2020-08-27 15:10:15 -0500

    Little change made in class.

--- src/main/scala/basics/HelloWorld.scala
+++ src/main/scala/basics/HelloWorld.scala
@@ -7,9 +7,10 @@ This is a basic main for you to start off with.
 */
 object HelloWorld {
 	def main(args: Array[String]): Unit = {
-    println("What is your name?")
-    val name = readLine().trim()
-    println(s"Hello $name!")
+		println("This is new.")
+		println("What is your name?")
+		val name = readLine().trim()
+		println(s"Hello $name!")
 	}
 	
 	def square(x: Double) = x*x
