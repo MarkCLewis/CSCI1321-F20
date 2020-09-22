ec37b40312a23af01180ba34390173cab371494a
Mark Lewis <mlewis@trinity.edu>
2020-09-22 13:20:26 -0500

    Queues are passing the tests.

--- src/main/scala/adt/ArrayQueue.scala
+++ src/main/scala/adt/ArrayQueue.scala
@@ -14,8 +14,14 @@ class ArrayQueue[A : ClassTag] extends MyQueue[A] {
   }
 
   def enqueue(a: A): Unit = {
-    // TODO: Grow the array if full
-    data(back)
+    if ((back + 1) % data.length == front) {
+      val tmp = Array.fill(data.length*2)(null.asInstanceOf[A])
+      for (i <- 0 until data.length-1) tmp(i) = data((front + i) % data.length)
+      front = 0
+      back = data.length - 1
+      data = tmp
+    }
+    data(back) = a
     back = (back + 1) % data.length
   }
 
---
+++ src/test/scala/adt/TestQueue.scala
@@ -0,0 +1,54 @@
+package adt
+
+import org.junit._
+import org.junit.Assert._
+
+class TestQueue {
+  var queue: adt.ArrayQueue[Int] = null
+
+  @Before def initQueue(): Unit = {
+    queue = new adt.ArrayQueue[Int]()
+  }
+
+  @Test def emptyOnCreate(): Unit = {
+    assertTrue(queue.isEmpty)
+  }
+
+  @Test def addRemoveOne(): Unit = {
+    queue.enqueue(5)
+    assertFalse(queue.isEmpty)
+    assertEquals(5, queue.peek)
+    assertEquals(5, queue.dequeue())
+    assertTrue(queue.isEmpty)
+  }
+
+  @Test def addRemoveMany(): Unit = {
+    val nums = Array.fill(100)(util.Random.nextInt)
+    nums.foreach(queue.enqueue(_))
+    assertFalse(queue.isEmpty)
+    for (n <- nums) {
+      assertEquals(n, queue.peek)
+      assertEquals(n, queue.dequeue())
+    }
+    assertTrue(queue.isEmpty)
+  }
+
+  @Test def addRemoveManyTwice(): Unit = {
+    val nums = Array.fill(100)(util.Random.nextInt)
+    nums.foreach(queue.enqueue(_))
+    assertFalse(queue.isEmpty)
+    for (n <- nums) {
+      assertEquals(n, queue.peek)
+      assertEquals(n, queue.dequeue())
+    }
+    assertTrue(queue.isEmpty)
+    nums.foreach(queue.enqueue(_))
+    assertFalse(queue.isEmpty)
+    for (n <- nums) {
+      assertEquals(n, queue.peek)
+      assertEquals(n, queue.dequeue())
+    }
+    assertTrue(queue.isEmpty)
+  }
+
+}
