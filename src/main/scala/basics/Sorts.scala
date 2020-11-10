a625683f16373411bc6c87c8cb71bef71f8afb0c
Mark Lewis <mlewis@trinity.edu>
2020-11-10 17:01:54 -0600

    11-10

--- src/main/scala/basics/Sorts.scala
+++ src/main/scala/basics/Sorts.scala
@@ -25,4 +25,29 @@ object Sorts extends App {
   println(nums.mkString(" "))
   bubbleSort2(nums)(_ > _)
   println(nums.mkString(" "))
+
+  def quicksort[A](lst: List[A])(lt: (A, A) => Boolean): List[A] = lst match {
+    case Nil | _ :: Nil => lst
+    case pivot :: rest =>
+      val (less, greater) = rest.partition(a => lt(a, pivot))
+      quicksort(less)(lt) ::: (pivot :: quicksort(greater)(lt))
+  }
+
+  println(quicksort(List.fill(10)(util.Random.nextInt(100)))(_ < _))
+
+  def merge[A](lst1: List[A], lst2: List[A])(lt: (A, A) => Boolean): List[A] = (lst1, lst2) match {
+    case (Nil, _) => lst2
+    case (_, Nil) => lst1
+    case (h1 :: t1, h2 :: t2) =>
+      if (lt(h1, h2)) h1 :: merge(t1, lst2)(lt) else h2 :: merge(lst1, t2)(lt)
+  }
+
+  def mergesort[A](lst: List[A])(lt: (A, A) => Boolean): List[A] = lst match {
+    case Nil | _ :: Nil => lst
+    case _ =>
+      val (left, right) = lst.splitAt(lst.length/2)
+      merge(mergesort(left)(lt), mergesort(right)(lt))(lt)
+  }
+
+  println(mergesort(List.fill(10)(util.Random.nextInt(100)))(_ < _))
 }
--- src/main/scala/basics/Sorts2.scala
+++ src/main/scala/basics/Sorts2.scala
@@ -34,4 +34,20 @@ object Sorts2 extends App {
   }
 
   println(quicksort(List.fill(20)(util.Random.nextInt(100)))(_<_))
+
+  def merge[A](lst1: List[A], lst2: List[A])(lt: (A, A) => Boolean): List[A] = (lst1, lst2) match {
+    case (Nil, _) => lst2
+    case (_, Nil) => lst1
+    case (h1 :: t1, h2 :: t2) =>
+      if (lt(h1, h2)) h1 :: merge(t1, lst2)(lt) else h2 :: merge(lst1, t2)(lt)
+  }
+
+  def mergesort[A](lst: List[A])(lt: (A, A) => Boolean): List[A] = lst match {
+    case Nil | _ :: Nil => lst
+    case _ =>
+      val (left, right) = lst.splitAt(lst.length/2)
+      merge(mergesort(left)(lt), mergesort(right)(lt))(lt)
+  }
+
+  println(mergesort(List.fill(20)(util.Random.nextInt(100)))(_<_))
 }
